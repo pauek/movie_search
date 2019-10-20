6c095ccdc7cb3a1819d41ef91283322724444259
Pau Fernández <pau.fernandez@upc.edu>
2019-10-20 17:16:04 +0200

    Ampliamos los campos de Movie

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -1,10 +1,29 @@
 class Movie {
-  String title;
+  int id;
+  String title, overview;
+  String originalTitle, originalLanguage;
+  String posterPath, backdropPath;
+  DateTime releaseDate;
+  List<int> genreIds;
+  num popularity;
+  int voteCount;
+  num voteAverage;
 
-  Movie.fromJson(Map<String, dynamic> json) : title = json['title'];
+  Movie.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        overview = json['overview'],
+        posterPath = json['poster_path'],
+        backdropPath = json['backdrop_path'],
+        genreIds = json['genre_ids'].cast<int>(),
+        popularity = json['popularity'],
+        voteCount = json['vote_count'],
+        voteAverage = json['vote_average'],
+        originalTitle = json['original_title'],
+        originalLanguage = json['original_language'];
 
   @override
-  String toString() => 'Movie(title: $title)';
+  String toString() => 'Movie(id: $id, title: $title, posterPath: $posterPath)';
 }
 
 class Actor {
