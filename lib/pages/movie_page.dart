4f016623345695e1b6422918bd8e04895e9da0e0
Pau Fernández <pau.fernandez@upc.edu>
2019-11-19 09:13:18 +0100

    Desarrollamos la página de la película un poco más

--- lib/pages/movie_page.dart
+++ lib/pages/movie_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_search/api/tmdb.dart' as api;
 import 'package:movie_search/model/movie.dart';
 import 'package:movie_search/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 class MoviePage extends StatelessWidget {
   @override
@@ -15,7 +16,10 @@ class MoviePage extends StatelessWidget {
           if (!snapshot.hasData) {
             return Loading();
           }
-          return _MoviePageBody(snapshot.data);
+          return Provider<Movie>.value(
+            value: snapshot.data,
+            child: _MoviePageBody(),
+          );
         },
       ),
     );
@@ -23,11 +27,9 @@ class MoviePage extends StatelessWidget {
 }
 
 class _MoviePageBody extends StatelessWidget {
-  final Movie movie;
-  const _MoviePageBody(this.movie);
-
   @override
   Widget build(BuildContext context) {
+    final Movie movie = Provider.of<Movie>(context);
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
@@ -45,14 +47,90 @@ class _MoviePageBody extends StatelessWidget {
         ),
         // if (movie != null)
         //   Image.network(api.imageUri(movie.backdropPath)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: _Poster(),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  flex: 2,
+                  child: _Header(),
+                ),
+              ],
+            ),
+          ),
+        ),
         SliverList(
           delegate: SliverChildListDelegate([
-            Text(movie.title),
-            Text(movie.overview),
-            Text(movie.genres.join(', ')),
+            _Overview(),
           ]),
-        ),
+        )
       ],
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Movie movie = Provider.of<Movie>(context);
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+      child: Text(movie.overview),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Movie movie = Provider.of<Movie>(context);
+    return Column(children: <Widget>[
+      SizedBox(height: 16),
+      _Title(),
+      SizedBox(height: 8),
+      Text(movie.genres.join(', ')),
+    ]);
+  }
+}
+
+class _Poster extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Movie movie = Provider.of<Movie>(context);
+    return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Material(
+        elevation: 10,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(api.imageUri(movie.posterPath)),
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Movie movie = Provider.of<Movie>(context);
+    return Text(
+      movie.title,
+      style: TextStyle(
+        fontSize: 26,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 }
